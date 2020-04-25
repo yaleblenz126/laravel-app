0050e34acff85e0006a2ac0a5b670414f3cf39a8
kazuaki nakazawa <kazu@knmac.local>
2020-04-25 09:45:37 +0900

    第二章 Complete!

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth; # 追加
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
@@ -12,11 +13,11 @@ class UserController extends Controller
       return view('user.signin');
     }
 
-    public function login(Request $request)
+    public function login(UserRequest $request)
   {
     $email    = $request->input('email');
     $password = $request->input('password');
-    
+
     if (!Auth::attempt(['email' => $email, 'password' => $password])) {
       // 認証失敗
       return redirect('/')->with('error_message', 'I failed to login');
@@ -24,4 +25,15 @@ class UserController extends Controller
     // 認証成功
     return redirect()->route('micropost.index');
   }
+
+      /**
+     * ログアウト処理アクション
+     */
+    public function logout()
+    {
+      Auth::logout();
+      return redirect()->route('user.signin');
+    }
+
+
 }
